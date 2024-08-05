1449afbb7157f756a55783fe5b006449c05d0fbb
TechCat <143054835+techcat-dev@users.noreply.github.com>
2024-08-04 20:18:34 -0400

    Create wormy.py

---
+++ python/wormy.py
@@ -0,0 +1,21 @@
+#Wormy - made by TechCat-Dev
+#This script either prints a "(" or a ")" in the terminal and repeats every 0.1 seconds until an interrupt.
+
+import time
+import random
+
+
+x = 0
+
+while (x == x):
+	time.sleep(0.1)
+	list1 = [1, 2]
+
+	x = (random.choice(list1))
+
+
+
+	if x == 1:
+	   print("(")
+	else:
+	   print(")")
